ccd5f6f955ba9e249c5130d8989aec25061dc00b
GlassJarGames <glassjargames@gmail.com>
2025-02-25 15:16:16 -0800

    Initial commit

---
+++ main.lua
@@ -0,0 +1,34 @@
+SMODS.Sound({
+    pitch = 1,
+    key = "music_bside_main",
+    path = "music_main_loop.ogg",
+    replace = 'music1',
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_bside_arcana",
+    path = "music_arcana_loop.ogg",
+    replace = 'music2',
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_bside_celestial",
+    path = "music_celestial_loop.ogg",
+    replace = 'music3',
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_bside_shop",
+    path = "music_shop_loop.ogg",
+    replace = 'music4',
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_bside_boss",
+    path = "music_boss_loop.ogg",
+    replace = 'music5',
+})
